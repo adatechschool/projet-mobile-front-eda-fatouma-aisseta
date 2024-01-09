e7f14bca5dfbd07c549ae3b3c41abfe77c4cdd3a
Aisséta <diaw.aisseta@gmail.com>
2024-01-09 11:59:41 +0100

    Initial Commit

---
+++ Projet Mobile/Projet_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_MobileApp.swift
+//  Projet Mobile
+//
+//  Created by Aisséta Diawara on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Projet_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projet MobileUITests/Projet_MobileUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Projet_MobileUITestsLaunchTests.swift
+//  Projet MobileUITests
+//
+//  Created by Aisséta Diawara on 09/01/2024.
+//
+
+import XCTest
+
+final class Projet_MobileUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
