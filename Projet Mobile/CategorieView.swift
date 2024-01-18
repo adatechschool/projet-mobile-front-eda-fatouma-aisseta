011200cb1890ed5208268465e4a6de88861f4ecf
Aisséta <diaw.aisseta@gmail.com>
2024-01-18 15:04:39 +0100

    categorie

--- Projet Mobile/CategorieView.swift
+++ Projet Mobile/CategorieView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct CategorieView: View {
+    @State var username: String = ""
+    @State var storyResume : String = ""
+    @State private var storyLength: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack{
+            LinearGradient(gradient: Gradient (colors: [ Color(.systemBlue)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack{
+                
+                NavigationView{
+                    
+                    Form{
+                        Section(header: Text("Longueur de l'histoire")) {
+                            Picker("Choisissez la longueur", selection: $storyLength) {
+                                Text("Courte").tag("Courte")
+                                Text("Longue").tag("Longue")
+                            }
+                            .pickerStyle(SegmentedPickerStyle())
+                        }
+                        Section("Choisir ton où tes personnages") {
+                            TextField("Username", text: $username)
+                            
+                        }
+                        Section("RÉSUMÉ DE L'HISTOIRE") {
+                            TextField("Ecrire ici...", text: $storyResume)
+                            
+                        }
+                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                            Text("Finalise et découvre l'histoire")
+                        })
+                        
+                        
+                        
+                    }
+                    
+                   
+                
+                    .navigationTitle(Text("Personnalise ton histoire"))
+                    
+                }
+                
+            }
+            
+            
+            
+        }
     }
+    
 }
 
 #Preview {
