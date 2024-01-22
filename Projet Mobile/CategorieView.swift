1e3a5c6e06996157bd0dbf02cb82c68b2dfb1d19
Eda OZDEMIR <edaozdemir@live.fr>
2024-01-22 16:45:39 +0100

    logique entre les vues + délai 5 secs sur WaitingScreen

--- Projet Mobile/CategorieView.swift
+++ Projet Mobile/CategorieView.swift
@@ -8,26 +8,22 @@
 import SwiftUI
 
 struct CategorieView: View {
-    @State var username: String = ""
-    @State var storyResume : String = ""
+    @State private var username: String = ""
+    @State private var storyResume: String = ""
     @State private var storyLength: String = ""
+    @State private var isNextViewActive: Bool = false //pour gérer la navigation
     @Environment(\.presentationMode) var presentationMode
     //pour dissoudre ou fermer une vue modale ou une vue présentée à l'aide de la navigation. Ensuite, elle appelle la méthode dismiss() pour fermer ou dissoudre la vue.
-    
 
 
-    
     var body: some View {
-        
-        ZStack{
-            LinearGradient(gradient: Gradient (colors: [ Color(.systemBlue)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color(.systemBlue)]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea(.all, edges: .all)
-            
-            VStack{
-                
-                NavigationView{
-                    
-                    Form{
+
+            VStack {
+                NavigationView {
+                    Form {
                         Section(header: Text("Longueur de l'histoire")) {
                             Picker("Choisissez la longueur", selection: $storyLength) {
                                 Text("Courte").tag("Courte")
@@ -35,47 +31,43 @@ struct CategorieView: View {
                             }
                             .pickerStyle(SegmentedPickerStyle())
                         }
+
                         Section("Choisir ton où tes personnages") {
                             TextField("Username", text: $username)
-                            
                         }
+
                         Section("RÉSUMÉ DE L'HISTOIRE") {
                             TextField("Ecrire ici...", text: $storyResume)
-                            
                         }
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                            Text("Finalise et découvre l'histoire")
-                        })
-                        
-                        
-                        
-                    }
-                    
-                   
-                
-                    .navigationTitle(Text("Personnalise ton histoire"))
-                    .navigationBarBackButtonHidden(true) // Cacher le bouton de retour par défaut
 
-                                        // Ajouter un bouton personnalisé de retour en arrière
-                                        .navigationBarItems(leading: Button(action: {
-                                            self.presentationMode.wrappedValue.dismiss()
-                                        }) {
-                                            Image(systemName: "arrow.left.circle.fill")
-                                                .resizable()
-                                                .frame(width: 30, height: 30)
-                                        })
-                    
+                        Button(action: {
+                            self.isNextViewActive.toggle()
+                        }) {
+                            Text("Finalise et découvre l'histoire")
+                        }
+                    }
+                    .navigationTitle("Personnalise ton histoire")
+        
+
+                    // Ajouter un bouton personnalisé de retour en arrière
+                    .navigationBarItems(leading: Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Image(systemName: "arrow.backward")
+                         //   .resizable()
+                         //   .frame(width: 30, height: 30)
+                    })
                 }
-                
             }
-            
-            
-            
+        }
+        .sheet(isPresented: $isNextViewActive) {
+            WaitingScreen()
         }
     }
-    
 }
 
-#Preview {
-    CategorieView()
+struct CategorieView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategorieView()
+    }
 }
